55c88dfa3a741b21482fd43139dcc53a0c8ac2c9
plena <zoelam020@gmail.com>
2019-07-02 11:36:13 +0800

    add WhiteBackgroundFlowLayout

---
+++ GSiOSKit/Classes/QRCodeScan/QRScanRow.swift
@@ -0,0 +1,83 @@
+//
+// QRScanRow.swift 
+// ACS-OM 
+// 
+// Created by Linzy on 2019/6/11. 
+// Copyright © 2019 Gosuncn. All rights reserved.
+// 
+
+import UIKit
+import Eureka
+import SnapKit
+import RxSwift
+import RxCocoa
+
+public class QRScanCell: _FieldCell<String>, CellType {
+    
+    let sendBtn : UIButton = {
+        let btn = UIButton()
+        return btn
+    }()
+    
+    open override func setup() {
+        super.setup()
+        selectionStyle = .none
+        addSubview(sendBtn)
+        sendBtn.snp.makeConstraints { (maker) in
+            maker.trailing.equalToSuperview().offset(-16)
+            maker.centerY.equalToSuperview()
+            maker.width.height.equalTo(48)
+        }
+        sendBtn.addTarget(self, action: #selector(sendAction), for: .touchUpInside)
+        textField.keyboardType = .asciiCapable
+    }
+    
+    open override func update() {
+        super.update()
+        textLabel?.textColor = UIColor.black
+        textField.textColor = UIColor.gray
+        textField.textAlignment = .left
+        textField.placeholder = "请扫描或输入\(row.title ?? "")"
+        sendBtn.isHidden = row.isDisabled
+    }
+    
+    let disposeBag = DisposeBag()
+    var timeDisposable : Disposable?
+
+    @objc func sendAction(){
+        if let navigationController = formViewController()?.navigationController {
+            row.value = ""
+            textField.text = ""
+            let scanViewController = setUPQRCodeScanViewController()
+            scanViewController.scanResultObservable.subscribe(onNext: { [weak self](result) in
+                if let scanRow = self?.row as? QRScanRow{
+                    scanRow.checkQRScanResult(qrcodeStr: result)
+                }
+            }).disposed(by: disposeBag)
+            navigationController.pushViewController(scanViewController, animated: true)
+        }
+    }
+    
+    open func setUPQRCodeScanViewController() -> QRCodeScanViewController {
+        let scanViewController = QRCodeScanViewController.init()
+        return scanViewController
+    }
+    
+    
+}
+
+open class QRScanRow: Row<QRScanCell> {
+    required public init(tag: String?) {
+        super.init(tag: tag)
+        cellProvider = CellProvider<QRScanCell>.init()
+    }
+    
+    
+    
+    open func checkQRScanResult(qrcodeStr: String){
+        
+    }
+    
+    
+}
+
---
+++ View/WhiteBackgroundFlowLayout.swift
@@ -0,0 +1,70 @@
+//
+// WhiteBackgroundFlowLayout.swift 
+// ACS-OM 
+// 
+// Created by Linzy on 2019/5/20. 
+// Copyright © 2019 Gosuncn. All rights reserved.
+// 
+
+
+import UIKit
+class WhiteBackgroundCollectionReusableView: UICollectionReusableView {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.backgroundColor = UIColor.white
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
+public class WhiteBackgroundFlowLayout: UICollectionViewFlowLayout {
+    override init() {
+        super.init()
+        register(WhiteBackgroundCollectionReusableView.self, forDecorationViewOfKind: "WhiteBackgroundCollectionReusableView")
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+//    override func layoutAttributesForDecorationView(ofKind elementKind: String, at indexPath: IndexPath) -> UICollectionViewLayoutAttributes?{
+//        let layoutAttributes = UICollectionViewLayoutAttributes.init(forDecorationViewOfKind: elementKind, with: indexPath)
+//        let lastIndex = collectionView?.numberOfItems(inSection: indexPath.section) ?? 0 - 1
+//        if lastIndex < 0 {
+//            return layoutAttributes
+//        }
+//        guard let firstItem = self.layoutAttributesForItem(at: IndexPath.init(row: 0, section: indexPath.section)),
+//        let lastItem = self.layoutAttributesForItem(at: IndexPath.init(row: lastIndex, section: indexPath.section)) else {
+//             return layoutAttributes
+//        }
+//
+//        layoutAttributes.zIndex = -1
+//        layoutAttributes.frame = CGRect.init(x: 0, y: firstItem.frame.minY,
+//                                             width: collectionViewContentSize.width,
+//                                             height: (lastItem.frame.origin.y - firstItem.frame.origin.y + firstItem.frame.size.height))
+//        return layoutAttributes
+//    }
+//    
+    override func layoutAttributesForElements(in rect: CGRect) -> [UICollectionViewLayoutAttributes]? {
+        guard let attributes = super.layoutAttributesForElements(in: rect) else{
+            return nil
+        }
+        var allAttributes = [UICollectionViewLayoutAttributes].init()
+        for attr in attributes{
+            if attr.representedElementCategory == UICollectionView.ElementCategory.cell,
+                attr.frame.origin.x == self.sectionInset.left{
+                let decorationAttributes = UICollectionViewLayoutAttributes.init(forDecorationViewOfKind:"WhiteBackgroundCollectionReusableView" ,
+                                                                                 with: attr.indexPath)
+                decorationAttributes.frame = CGRect.init(x: 0, y: attr.frame.origin.y,
+                                                         width: collectionViewContentSize.width, height: attr.frame.size.height)
+                decorationAttributes.zIndex =  -1
+                allAttributes.append(decorationAttributes)
+                
+            }
+        }
+        allAttributes.append(contentsOf: attributes)
+        return allAttributes
+    }
+}
